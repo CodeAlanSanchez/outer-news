12f26f2bdf2087e08b5459fe4ec214d450c1c21a
Alan Sanchez <realalansan@gmail.com>
2021-02-25 22:27:23 -0800

    Update Hero and Article component

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -7,7 +7,7 @@ const Article = ({ article }: {
     summary: string,
     url: string}}) => (
       <article className="space-article">
-        <a href={article.url}>
+        <a target="_blank" rel="noreferrer" href={article.url}>
           <img className="image" src={article.imageUrl} alt={article.title} />
           <h1 className="title">{article.title}</h1>
           <p className="date">{article.publishDate}</p>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,10 +4,13 @@ const Hero = () => (
   <div className="hero">
     <div>
       <h1>Outer News</h1>
-      <p>Using an API from </p>
-      <span><a href="https://www.spaceflightnow.com/">Space Flight News</a></span>
+      <p>Articles take you to </p>
+      <span><a target="_blank" rel="noreferrer" href="https://www.spaceflightnow.com/">Space Flight News</a></span>
       <hr />
-      <p><em>made using ReactJs and Redux</em></p>
+      <p>
+        <em>made using ReactJs and Redux with an </em>
+        <span><em><a className="api" target="_blank" rel="noreferrer" href="https://www.spaceflightnewsapi.net/documentation">API</a></em></span>
+      </p>
     </div>
   </div>
 );
