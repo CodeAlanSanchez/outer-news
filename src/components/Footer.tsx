a6bd5d109b68aefc36840c4f17b7b5d01aadd136
Alan Sanchez <realalansan@gmail.com>
2021-05-21 10:16:02 -0700

    Fix portfolio link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => (
   <div className="footer" id="contact">
     <h1>Contact Me</h1>
     <p className="email">realalansan@gmail.com</p>
-    <a className="portfolio link" href="http://localhost:3000">Portfolio</a>
+    <a className="portfolio link" href="http://www.alansanchez.dev">Portfolio</a>
   </div>
 );
 
