6f548daa03b8f05cc3be16af6602096122f2b613
Alan Sanchez <realalansan@gmail.com>
2021-08-22 07:53:18 -0700

    Add box shadow

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 const About = () => (
-  <div className="about background-parallax shadow" id="about">
-    <h1>About</h1>
-    <p>
-      This website was built using React, Redux, Typescript, and an
-      {' '}
-      <span><a className="link" href="https://www.spaceflightnewsapi.net/documentation">API</a></span>
-      .
-      It shows several articles and a summary of them, they link to the original website.
-    </p>
+  <div className="about background-parallax" id="about">
+    <div className="box-shadow">
+      <h1>About</h1>
+      <p>
+        This website was built using React, Redux, Typescript, and an
+        {' '}
+        <span><a className="link" href="https://www.spaceflightnewsapi.net/documentation">API</a></span>
+        .
+        It shows several articles and a summary of them, they link to the original website.
+      </p>
+    </div>
   </div>
 );
 
