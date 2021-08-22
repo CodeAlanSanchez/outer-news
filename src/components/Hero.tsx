0f403178116698460623cd98b2f657007e5334ea
Alan Sanchez <realalansan@gmail.com>
2021-08-22 06:37:49 -0700

    Update hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Hero = () => (
   <div className="hero background-parallax shadow" id="hero">
     <div>
       <h1>Outer News</h1>
-      <p>Articles take you to </p>
-      <span><a target="_blank" rel="noreferrer" className="link" href="https://www.spaceflightnow.com/">Space Flight News</a></span>
+      <p>Articles take you to third-party site</p>
       <hr />
       <p>
-        <em>made using ReactJs and Redux with an </em>
-        <span><em><a className="link" target="_blank" rel="noreferrer" href="https://www.spaceflightnewsapi.net/documentation">API</a></em></span>
+        <em>made using ReactJS with Redux + TypeScript and an </em>
+        <span>
+          <em>
+            <a
+              className="link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.spaceflightnewsapi.net/documentation"
+            >
+              API
+            </a>
+          </em>
+        </span>
       </p>
     </div>
   </div>
