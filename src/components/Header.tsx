e2187fdf140efc461773f0f005a05ded9f99c671
Alan Sanchez <realalansan@gmail.com>
2021-08-22 07:53:29 -0700

    Remove contact section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,6 @@ const Header = () => (
       <ul>
         <li><a href="#hero">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
   </header>
