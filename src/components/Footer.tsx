7b8fd3cf0348a470c832551c525d210706bc2476
Alan Sanchez <realalansan@gmail.com>
2021-08-22 06:37:55 -0700

    Remove footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Footer = () => (
-  <div className="footer" id="contact">
-    <h1>Contact Me</h1>
-    <p className="email">realalansan@gmail.com</p>
-    <a className="portfolio link" href="http://www.alansanchez.dev">Portfolio</a>
-  </div>
+  <>
+
+  </>
+  // <div className="footer" id="contact">
+  //   <h1>Contact Me</h1>
+  //   <p className="email">realalansan@gmail.com</p>
+  //   <a className="portfolio link" href="http://www.alansanchez.dev">Portfolio</a>
+  // </div>
 );
 
 export default Footer;
